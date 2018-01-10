533711a41ae4283bf7b0efee146bcb1f0f5a2c1a
gabriel <gabrielcordeiro@alunos.utfpr.edu.br>
2018-01-09 22:20:46 -0200

    melhora de execução

--- src/DAO/statemens.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS schedule;
-CREATE TABLE schedule (
-  idOperacao SERIAL,
-  indiceTransacao INTEGER,
-  operacao VARCHAR(10),
-  itemDado VARCHAR(10),
-  timestampj VARCHAR(15),
-  CONSTRAINT pk_constraint PRIMARY KEY (idOperacao)
-)
